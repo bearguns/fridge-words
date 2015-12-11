86ecbb0e2c4c1c0503e97516b2a2259f8bbc5cb0
Sean Brage <seanmbrage@me.com>
2015-12-11 15:45:31 -0700

    random word retrieval working

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,32 @@
-var freezer = #(".freezer");
-var fridge = #(".fridge");
+$(document).ready(function() {
+  var freezer = $(".freezer");
+  var fridge = $(".fridge");
+  var titlecard = $(".titlecard")
+  var start = $("#start");
+
+  linkTest();
+
+  $("#start").click(function() {
+    ajaxcall();
+  });
+
+
+function linkTest() {
+  console.log('js files linked!');
+}
+
+function ajaxcall() {
+  var ajaxdata = $.ajax({
+    url: 'https://wordsapiv1.p.mashape.com/words/?random=true',
+    method: "GET",
+    datatype: "json",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-Mashape-Authorization", "fuLpcihFgjmshxoWGAv5FDdYHC4kp1fOnKRjsnD03CdkmExrvu"); // Enter here your Mashape key
+    },
+    success: function(response) {
+      freezer.append("<p>" + response['word'] + "</p>");
+    }
+  })
+}
+
+});

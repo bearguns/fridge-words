ef04eb28c0bd8215cf240717511e3e78a8cf560f
Sean Brage <seanmbrage@me.com>
2015-12-14 08:23:54 -0700

    words are displayed on the page on button click

--- js/dom.js
+++ js/dom.js
@@ -3,33 +3,14 @@ $(document).ready(function() {
   var fridge = $(".fridge");
   var titlecard = $(".titlecard")
   var start = $("#start");
-  var wordList = [];
-
-  linkTest();
-
-  $("#start").click(function() {
-    ajaxcall();
+  $(start).click(function(){
+    writeWords(wordList);
   });
 
-
-function linkTest() {
-  console.log('js files linked!');
-}
-
-var ajaxi = $.ajax({
-  url: "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=50&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=78598b23ab7389d7d520b0972b20221b03e702bf7aba96083",
-  method: "GET",
-  dataType: "json"
-});
-
-ajaxi.done(function(response){
-  response.forEach(function(i){
-    wordList.push(i);
-  });
-  wordList.forEach(function(i){
-    console.log(i.word);
-  });
-});
-
+  function writeWords(array) {
+    array.forEach(function(i){
+      fridge.append("<p>" + i.word + "</p>");
+    });
+  }
 
 });
---
+++ js/logic.js
@@ -0,0 +1,17 @@
+var wordList = [];
+// make ajax call
+var ajaxi = $.ajax({
+  url: "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=1&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=78598b23ab7389d7d520b0972b20221b03e702bf7aba96083",
+  method: "GET",
+  dataType: "json"
+});
+// ajax call response capture
+ajaxi.done(function(response){
+  response.forEach(function(i){
+    wordList.push(i);
+  });
+  wordList.forEach(function(i){
+    console.log(i.word);
+  });
+});
+// write words to page

fe1a9e8321004713eabbdfe3a370e99e2219cfff
Sean Brage <seanmbrage@me.com>
2015-12-14 11:17:24 -0700

    dragula working to move between containers. add some prepositions.

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
-  $(document).on('click',function (){
-  $(".fridge > p").draggable().sortable();
-});
+  dragula([document.querySelector(".freezer"), document.querySelector(".fridge")]);
   var freezer = $(".freezer");
   var fridge = $(".fridge");
   var titlecard = $(".titlecard")
   var start = $("#start");
   $(start).click(function(){
+    writePreps(makeRandom(), fridge);
     writeWords(wordList);
   });
-
   function writeWords(array) {
     array.forEach(function(i){
       fridge.append("<p>" + i.word + "</p>");
--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,8 @@
 var wordCount = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
-console.log(wordCount);
 var wordList = [];
+function makeRandom() {
+  return Math.floor(Math.random() * (4 - 2 + 1)) + 1;
+};
 // make ajax call
 var ajaxi = $.ajax({
   url: "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=1&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit="+wordCount+"&api_key=78598b23ab7389d7d520b0972b20221b03e702bf7aba96083",
@@ -18,3 +20,13 @@ ajaxi.done(function(response){
   });
 });
 // write words to page
+function writePreps(num, container) {
+  while (num > 0) {
+    $(container).append("<p>the</p>");
+    $(container).append("<p>while</p>");
+    $(container).append("<p>can\'t</p>");
+    $(container).append("<p>did</p>");
+    $(container).append("<p>not</p>");
+    num--;
+  }
+}

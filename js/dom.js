7ca6084061ecad682b49323734fef848b4936851
Sean Brage <seanmbrage@me.com>
2015-12-14 08:14:07 -0700

    successful test of wordnik api. able to retrieve multiple random words, specify frequency of use, and capture in array.

--- js/dom.js
+++ js/dom.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var fridge = $(".fridge");
   var titlecard = $(".titlecard")
   var start = $("#start");
+  var wordList = [];
 
   linkTest();
 
@@ -15,18 +16,20 @@ function linkTest() {
   console.log('js files linked!');
 }
 
-function ajaxcall() {
-  var ajaxdata = $.ajax({
-    url: 'https://wordsapiv1.p.mashape.com/words/?random=true',
-    method: "GET",
-    datatype: "json",
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader("X-Mashape-Authorization", "fuLpcihFgjmshxoWGAv5FDdYHC4kp1fOnKRjsnD03CdkmExrvu"); // Enter here your Mashape key
-    },
-    success: function(response) {
-      freezer.append("<p>" + response['word'] + "</p>");
-    }
-  })
-}
+var ajaxi = $.ajax({
+  url: "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=50&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=78598b23ab7389d7d520b0972b20221b03e702bf7aba96083",
+  method: "GET",
+  dataType: "json"
+});
+
+ajaxi.done(function(response){
+  response.forEach(function(i){
+    wordList.push(i);
+  });
+  wordList.forEach(function(i){
+    console.log(i.word);
+  });
+});
+
 
 });
